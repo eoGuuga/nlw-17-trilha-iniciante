f5aeaf5b5353dfdc3791b11035b56ff700489add
Gustavo Henrick <gustavo.hrsilva15@gmail.com>
2024-09-09 22:44:55 -0300

    fundamento003

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = {
+    value: 'correr muito',
+    checked: false,
+}
+
+let metas = [
+    meta,
+    {
+        value: 'nao correr nada',
+        checked: true
+    }
+]
+
+console.log(metas[1].value)
